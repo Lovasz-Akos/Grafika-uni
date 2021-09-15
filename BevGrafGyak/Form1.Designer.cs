5c8ec1a247c2a75eed1d4d249ca3b4d48c8d335a
Lovász Ákos <akos4x44@gmail.com>
2021-09-15 12:14:30 +0200

    added canvas and some mouse control methods

--- BevGrafGyak/Form1.Designer.cs
+++ BevGrafGyak/Form1.Designer.cs
@@ -28,13 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.canvas = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // canvas
+            // 
+            this.canvas.Location = new System.Drawing.Point(12, 12);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(734, 551);
+            this.canvas.TabIndex = 0;
+            this.canvas.TabStop = false;
+            this.canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.canvas_Paint);
+            this.canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseDown);
+            this.canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseMove);
+            this.canvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(922, 575);
+            this.Controls.Add(this.canvas);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox canvas;
     }
 }
 
--- BevGrafGyak/Form1.cs
+++ BevGrafGyak/Form1.cs
@@ -12,9 +12,30 @@ namespace BevGrafGyak
 {
     public partial class Form1 : Form
     {
+        Graphics g;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void canvas_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void canvas_MouseUp(object sender, MouseEventArgs e)
+        {
+
         }
     }
 }
