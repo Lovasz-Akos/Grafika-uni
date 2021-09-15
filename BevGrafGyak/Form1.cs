f46d272b6db235d82c91ea885e8b60a824a4fcf4
Lovász Ákos <akos4x44@gmail.com>
2021-09-15 12:53:11 +0200

    added square mover thing

--- BevGrafGyak/Form1.cs
+++ BevGrafGyak/Form1.cs
@@ -13,6 +13,10 @@ namespace BevGrafGyak
     public partial class Form1 : Form
     {
         Graphics g;
+        PointF P = new PointF();
+        float size = 100;
+        bool gotcha;
+        Brush brushSquare = new SolidBrush(Color.Red);
         public Form1()
         {
             InitializeComponent();
@@ -21,21 +25,32 @@ namespace BevGrafGyak
         private void canvas_Paint(object sender, PaintEventArgs e)
         {
             g = e.Graphics;
+
+            g.FillRectangle(brushSquare, P.X, P.Y, size, size);
         }
 
         private void canvas_MouseDown(object sender, MouseEventArgs e)
         {
-
+            if (P.X <= e.X && e.X < P.X+size&&P.Y<=e.Y&&e.Y<P.Y+size)
+            {
+                gotcha = true;
+            }
         }
 
         private void canvas_MouseMove(object sender, MouseEventArgs e)
         {
+            if (gotcha)
+            {
+                P.X = e.X;
+                P.Y = e.Y;
 
+                canvas.Invalidate();
+            }
         }
 
         private void canvas_MouseUp(object sender, MouseEventArgs e)
         {
-
+            gotcha = false;
         }
     }
 }
