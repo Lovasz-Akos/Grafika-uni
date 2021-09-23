8ee8c28f9ae0c286020122c79523027daff76477
Lovász Ákos <akos4x44@gmail.com>
2021-09-23 12:24:19 +0200

    line drawing test

--- BevGrafGyak/Form1.cs
+++ BevGrafGyak/Form1.cs
@@ -14,6 +14,9 @@ namespace BevGrafGyak
     public partial class Form1 : Form
     {
         Graphics g;
+
+        PointF p1 = new PointF(100, 20);
+        PointF p2 = new PointF(300, 500);
         public Form1()
         {
             InitializeComponent();
@@ -22,6 +25,8 @@ namespace BevGrafGyak
         private void canvas_Paint(object sender, PaintEventArgs e)
         {
             g = e.Graphics;
+
+            g.DrawLineDDA(Color.Red, Color.Blue, p1, p2);
         }
 
         private void canvas_MouseDown(object sender, MouseEventArgs e)
