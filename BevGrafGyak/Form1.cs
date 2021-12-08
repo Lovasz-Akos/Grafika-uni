31f05a183d75abb061a7a07894beb3c7346d8158
Lovász Ákos <akos4x44@gmail.com>
2021-12-08 22:24:38 +0100

    moved images to debug folder

--- BevGrafGyak/Form1.cs
+++ BevGrafGyak/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Windows.Forms;
 
@@ -16,17 +17,25 @@ namespace BevGrafGyak
         int tileSize = 100;
 
         Rectangle[,] tiles = new Rectangle[4, 4];
-        String[] pictureTitles = new string[] { "calculator", "diamond", "fish", "hotdog", "orange", "pyramid", "sun", "viking" };
+        Rectangle[,] pictureTiles = new Rectangle[4, 4];
 
+        String[,] pictures = new String[4, 4];
+
+        String[] pictureTitles = new String[] { "calculator", "diamond", "fish", "hotdog", "orange", "pyramid", "sun", "viking", "calculator", "diamond", "fish", "hotdog", "orange", "pyramid", "sun", "viking" };
+
+        Random rng;
         public Form1()
         {
             InitializeComponent();
             CreateTileGridStruct();
+            GeneratePictureGrid();
+            GeneratePictureTileGrid();
         }
 
         private void canvas_Paint(object sender, PaintEventArgs e)
         {
             g = e.Graphics;
+            DrawAllPictures(g);
             DrawTileGrid(g);
             FillListboxWithTiles();
 
@@ -43,6 +52,70 @@ namespace BevGrafGyak
             }
         }
 
+        private void GeneratePictureTileGrid()
+        {
+            int k = 0;
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    pictureTiles[i, j] = new Rectangle(tileGridStartX + (j * 100) + (j * 10), tileGridStartY + (i * 100) + (i * 10), tileSize, tileSize);
+                }
+            }
+            rng = new Random();
+            Shuffle(rng, pictures);
+        }
+        private void GeneratePictureGrid()
+        {
+            int k = 0;
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    pictures[i, j] = pictureTitles[k];
+                    k++;
+                }
+            }
+            rng = new Random();
+            Shuffle(rng, pictures);
+        }
+
+        private void DrawAllPictures(Graphics g)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    try
+                    {
+                        g.DrawImage(Image.FromFile(Path.Combine(Application.StartupPath, "Images\\" + pictures[i, j] + ".png")), pictureTiles[i, j]);
+                    }
+                    catch { };
+                }
+            }
+        }
+
+        public static void Shuffle<T>(Random random, T[,] array)
+        {
+            int lengthRow = array.GetLength(1);
+
+            for (int i = array.Length - 1; i > 0; i--)
+            {
+                int i0 = i / lengthRow;
+                int i1 = i % lengthRow;
+
+                int j = random.Next(i + 1);
+                int j0 = j / lengthRow;
+                int j1 = j % lengthRow;
+
+                T temp = array[i0, i1];
+                array[i0, i1] = array[j0, j1];
+                array[j0, j1] = temp;
+            }
+        }
+        //Fisher-Yates shuffle from https://stackoverflow.com/questions/30164019/shuffling-2d-array-of-cards
 
         private void DrawTileGrid(Graphics g)
         {
@@ -65,12 +138,16 @@ namespace BevGrafGyak
                 {
                     if (((e.Location.X > tiles[i, j].X) && (e.Location.X < tiles[i, j].X + tileSize)) && (e.Location.Y > tiles[i, j].Y) && (e.Location.Y < tiles[i, j].Y + tileSize))
                     {
-                        return new int[] { i, j };
+                        return new int[] { i, j, 0 };
+                    }
+                    else if (((e.Location.X > pictureTiles[i, j].X) && (e.Location.X < pictureTiles[i, j].X + tileSize)) && ((e.Location.Y > pictureTiles[i, j].Y) && (e.Location.Y < pictureTiles[i, j].Y + tileSize)))
+                    {
+                        return new int[] { i, j, 1 };
                     }
                 }
 
             }
-            return new int[] { -1, -1 };
+            return new int[] { -1, -1, -1 };
         }
 
         private void canvas_MouseMove(object sender, MouseEventArgs e)
@@ -82,7 +159,15 @@ namespace BevGrafGyak
         {
             int[] idS = new int[2];
             idS = GetClickedTileID(e);
-            DeleteTile(idS[0], idS[1]);
+            //MessageBox.Show(idS[0].ToString() + "  " + idS[1].ToString());
+            if (idS[2] == 0)
+            {
+                ShowIMG(idS[0], idS[1]);
+            }
+            else if (idS[2] == 1)
+            {
+                HideIMG(idS[0], idS[1]);
+            }
         }
 
 
@@ -107,7 +192,8 @@ namespace BevGrafGyak
 
         private void ShowIMG(int tileID_X, int tileID_Y)
         {
-            //TODO: show image and draw bounding box
+            DeleteTile(tileID_X, tileID_Y);
+            canvas.Invalidate(true);
         }
 
         private void HideIMG(int tileID_X, int tileID_Y)
@@ -128,6 +214,8 @@ namespace BevGrafGyak
         private void StartButton_Click(object sender, EventArgs e)
         {
             CreateTileGridStruct();
+            rng = new Random();
+            Shuffle(rng, pictures);
             canvas.Invalidate();
         }
     }
